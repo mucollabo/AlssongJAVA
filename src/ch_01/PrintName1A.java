e9230c136a5e7f516a6ffa1a6185a4a89b64507e
Charles An <mucollabo@gmail.com>
2020-04-17 18:29:46 +0900

    until study chapter 01

---
+++ src/ch_01/PrintName1A.java
@@ -0,0 +1,7 @@
+public class PrintName1A {
+    public static void main(String[] args) {
+        System.out.println("안");
+        System.out.println("철");
+        System.out.println("현");
+    }
+}
---
+++ src/ch_01/PrintName1B.java
@@ -0,0 +1,5 @@
+public class PrintName1B {
+    public static void main(String[] args) {
+        System.out.println("안\n철\n현");
+    }
+}
